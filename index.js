888eda4024e1c3a15879d8a859b19d6f0e282b00
Pilla Julien <vrlitwhai.kridanik@gmail.com>
2017-06-18 09:58:17 +0200

    fix dependencies

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const schema = require('./src/schema');
 const {startSenecaClient, resolvers} = require('./src/resolvers');
 
+
 function startGraphQLServer(schema, rootResolvers, context) {
   const app = express();
   startSenecaClient();
@@ -14,13 +15,7 @@ function startGraphQLServer(schema, rootResolvers, context) {
         context: context,
         graphiql: true,
         pretty: true,
-        formatError: error => ({
-          message: error.message,
-          locations: error.locations,
-          stack: error.stack,
-          path: error.path
-        }),
   })).listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
-};
+}
 
 startGraphQLServer(schema, resolvers, {});
